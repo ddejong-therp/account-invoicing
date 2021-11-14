f199a6af615b19e08b93e9fc11ce82204d85bd5c
Danny de Jong <ddejong@therp.nl>
2021-11-14 21:05:18 +0100

    [DEL] [no task] Removed unused file.

--- account_invoice_analytic_line/models/__init__.py
+++ account_invoice_analytic_line/models/__init__.py
@@ -2,8 +2,7 @@
 # Copyright 2018 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from . import account_config_settings
-from . import account_move_analytic_line_discount
 from . import account_analytic_line
-from . import account_invoice_line
+from . import account_move_analytic_line_discount
 from . import project_project
 from . import res_partner
